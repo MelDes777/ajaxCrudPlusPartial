d86049b290096874701a39b6dceff3f2ea324718
Strakatov Nikolay <desux327@gmail.com>
2023-01-26 00:16:03 +0600

    Add project files.

---
+++ AdvancedAjax/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace AdvancedAjax.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
